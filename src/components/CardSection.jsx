b6e56cced8d392ff834839a7fad47c6a3be1c773
Sibindra <timalsinasibndra@gmail.com>
2022-11-17 20:27:47 +0545

    props now work

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import Card from "./Card";
 
-import datas from "../data.json"
+import datas from "../data.json";
 
 export default function CardSection() {
-        return (
-            datas.map(data=>{
+    return datas.map((data) => {
+        return(
+
             <div id="CardSection">
-                <Card
-                    img={data.img}
-                    location={data.location}
-                    locationLink={data.locationLink}
-                    title={data.title}
-                    date={data.title}
-                    desc={data.desc}
+            <Card
+                img={data.img}
+                location={data.location}
+                locationLink={data.locationLink}
+                title={data.title}
+                date={data.title}
+                desc={data.desc}
                 />
-            </div>
-            })
-        );
+        </div>
+    )
+    });
 }
