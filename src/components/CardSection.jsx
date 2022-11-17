4443e4e63178196373ae004ef05ae942ceccfbb8
Sibindra <timalsinasibndra@gmail.com>
2022-11-17 20:16:34 +0545

    added data to data.json

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Card from "./Card";
 
+import datas from "../data.json"
+
 export default function CardSection() {
         return (
+            datas.map(data=>{
             <div id="CardSection">
                 <Card
-                    img="https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=999&q=80"
-                    location="Kathmandu"
-                    locationLink="#"
-                    title="Burger"
-                    date="15 Nov"
-                    desc="hello world"
+                    img={data.img}
+                    location={data.location}
+                    locationLink={data.locationLink}
+                    title={data.title}
+                    date={data.title}
+                    desc={data.desc}
                 />
             </div>
+            })
         );
 }
